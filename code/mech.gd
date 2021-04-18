6740d3162cdac9b1aa55ff487774340fae0f3bcf
Z53-INCOMING <75651517+Z53-INCOMING@users.noreply.github.com>
2021-04-18 14:26:50 -0700

    i finished part 2 of the enemy mech
    it was hard, tedious, and annoying. but thanks to the help of my dad, it was doable and we got it done.

--- code/mech.gd
+++ code/mech.gd
@@ -1,24 +1,10 @@
 extends KinematicBody2D
 class_name Mech
 
-onready var health_loss_particle = $"UI/health loss particle"
+
 onready var regen_timer = $"regen timer"
 onready var art = $color
-onready var food_bar = $UI/food
-onready var health_bar = $UI/health
-onready var UI = $UI
-onready var XP_bar = $UI/XP
-onready var XP_label = $UI/XPLabel
-onready var max_health_bar = $"UI/max health"
-onready var bullet_speed_bar = $"UI/bullet speed"
-onready var bullet_damage_bar = $"UI/bullet damage"
-onready var bullet_reload_bar = $"UI/bullet reload"
-onready var hunger_endurance_bar = $"UI/hunger endurance"
-onready var chisel_speed_bar = $"UI/chisel speed"
-onready var speed_bar = $"UI/speed"
-onready var stat_coin_label = $"UI/stat coin label"
-onready var back_miner_evolution = $"UI/back miner evolution"
-onready var twin_miner_evolution = $"UI/twin miner evolution"
+
 onready var chisel1 = $color/Chisel
 onready var chisel2 = $color/Chisel2
 onready var chisel3 = $color/Chisel3
@@ -30,14 +16,6 @@ onready var auto_turret1 = $"color/auto turret"
 onready var auto_turret2 = $"color/auto turret2"
 onready var cannon1 = $color/cannon
 onready var cannon2 = $color/cannon2
-onready var sniper_back_evolution = $"UI/sniper back evolution"
-onready var auto_miner_evolution = $"UI/auto miner evolution"
-onready var gunner_back_evolution = $"UI/gunner back evolution"
-onready var quadro_dig_evolution = $"UI/quadro dig evolution"
-onready var teamwork_evolution = $"UI/teamwork evolution"
-onready var twin_sniper_evolution = $"UI/twin sniper evolution"
-onready var rocketship_evolution = $"UI/rocketship evolution"
-onready var excavator_evolution = $"UI/excavator evolution"
 onready var skin = $color/circle
 
 var team = ""
@@ -110,14 +88,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	past_health = health
 	if Input.is_action_pressed("ui_accept"):
-		XP += 20
-	max_health_bar.value = max_health
-	bullet_speed_bar.value = bullet_speed
-	bullet_damage_bar.value = bullet_damage
-	bullet_reload_bar.value = bullet_reload
-	hunger_endurance_bar.value = hunger_endurance
-	chisel_speed_bar.value = chisel_speed
-	speed_bar.value = speed
+		XP += 99
+	
 	
 	if Input.is_action_just_pressed("auto fire"):
 		auto_fire = !auto_fire
@@ -127,10 +99,7 @@ func _process(delta: float) -> void:
 		auto_mine = !auto_mine
 	if Input.is_action_just_pressed("flip"):
 		flipped = !flipped
-	health_bar.value = health
-	food_bar.value = food
-	XP_bar.value = XP
-	XP_label.text = str(level)
+	
 	var checky = true
 	var checkx = true
 	if Input.is_action_pressed("left"):
@@ -159,7 +128,7 @@ func _process(delta: float) -> void:
 		rotation_degrees = rotation_degrees - 180
 	skin.rotation = 0 - rotation
 	
-	stat_coin_label.text = str(stat_coins)
+	
 	
 	if Input.is_action_pressed("mine") or auto_mine:
 		for chisel in get_tree().get_nodes_in_group("Chisels"):
@@ -194,39 +163,8 @@ func _process(delta: float) -> void:
 		if mobility < abs(velocity.y):
 			velocity.y = sign(velocity.y) * mobility
 	velocity = move_and_slide(velocity, Vector2.UP, true, 4, 0, false)
-	health_bar.max_value = (max_health * 10) + 100
-	food_bar.max_value = (hunger_endurance * 10) + 100
-	if food > food_bar.max_value:
-		food = food_bar.max_value
-	if XP >= XP_bar.max_value and XP_label.text != str(30):
-		XP = XP - XP_bar.max_value
-		
-		XP_bar.max_value *= 1.1
-		XP_bar.max_value = round(XP_bar.max_value)
-		level += 1
-		stat_coins += 1
-		$UI/Xp_particle.emitting = true
-		
-		if level == 10:
-			twin_miner_evolution.visible = true
-			back_miner_evolution.visible = true
-		if level == 20:
-			if mech_type == "back miner":
-				sniper_back_evolution.visible = true
-				gunner_back_evolution.visible = true
-				auto_miner_evolution.visible = true
-			else:
-				teamwork_evolution.visible = true
-				quadro_dig_evolution.visible = true
-				auto_miner_evolution.visible = true
-		if level == 30:
-			if mech_type == "sniper back":
-				rocketship_evolution.visible = true
-				twin_sniper_evolution.visible = true
-			if mech_type == "quadro dig":
-				excavator_evolution.visible = true
-			XP_label.visible = false
-			XP_bar.visible = false
+	
+	
 			
 
 func _on_Timer_timeout() -> void:
@@ -242,60 +180,22 @@ func _on_Chisel_daimond() -> void:
 func _on_health_timer_timeout() -> void:
 	if food <= 0:
 		health -= 1
-		health_loss_particle.emitting = true
-	else:
-		health_loss_particle.emitting = false
-func _on_chisel_speed_button_button_down() -> void:
-	if stat_coins > 0 and chisel_speed != 10:
-		chisel_speed += 1
-		stat_coins -= 1
-func _on_hunger_endurance_button_button_down() -> void:
-	if stat_coins > 0 and hunger_endurance != 10:
-		hunger_endurance += 1
-		stat_coins -= 1
-		food += 10
-func _on_bullet_reload_button_button_down() -> void:
-	if stat_coins > 0 and bullet_reload != 10:
-		bullet_reload += 1
-		stat_coins -= 1
-func _on_bullet_damage_button_button_down() -> void:
-	if stat_coins > 0 and bullet_damage != 10:
-		bullet_damage += 1
-		stat_coins -= 1
-func _on_bullet_speed_button_button_down() -> void:
-	if stat_coins > 0 and bullet_speed != 10:
-		bullet_speed += 1
-		stat_coins -= 1
-func _on_speed_button_button_down() -> void:
-	if stat_coins > 0 and speed != 10:
-		speed += 1
-		stat_coins -= 1
-func _on_max_health_button_button_down() -> void:
-	if stat_coins > 0 and max_health != 10:
-		max_health += 1
-		stat_coins -= 1
-		health += 10
-func _on_twin_miner_evolution_button_down() -> void:
-	twin_miner_evolution.visible = false
-	back_miner_evolution.visible = false
+
+func evolve_twin_miner() -> void:
 	chisel1.position = Vector2(13, -15)
 	chisel2.position = Vector2(12.7, 12.3)
 	chisel2.visible = true
 	chisel2.rotation_degrees = 0
 	mech_type = "twin miner"
-func _on_back_miner_evolution_button_down() -> void:
-	twin_miner_evolution.visible = false
-	back_miner_evolution.visible = false
+func evolve_back_miner() -> void:
+	
 	chisel1.position = Vector2(-19, 0)
 	chisel1.rotation_degrees = 176.5
 	cannon1.visible = true
 	cannon1.position = Vector2(22.3, -0.4)
 	cannon1.scale.x = 1.3
 	mech_type = "back miner"
-func _on_sniper_back_evolution_button_down() -> void:
-	sniper_back_evolution.visible = false
-	gunner_back_evolution.visible = false
-	auto_miner_evolution.visible = false
+func evolve_sniper_back() -> void:
 	cannon1.position = Vector2(22.3, -0.4)
 	cannon1.rotation_degrees = 0
 	cannon1.scale = Vector2(2.3, 1.3)
@@ -303,10 +203,8 @@ func _on_sniper_back_evolution_button_down() -> void:
 	cannon1.reload = 0.8
 	cannon1.damage = 1.6
 	mech_type = "sniper back"
-func _on_gunner_back_evolution_button_down() -> void:
-	sniper_back_evolution.visible = false
-	gunner_back_evolution.visible = false
-	auto_miner_evolution.visible = false
+func evolve_gunner_back() -> void:
+	
 	spread_cannon1.position = Vector2(24, -0.6)
 	spread_cannon1.reload = 1.4
 	spread_cannon1.visible = true
@@ -314,12 +212,8 @@ func _on_gunner_back_evolution_button_down() -> void:
 	spread_cannon1.damage = 0.8
 	mech_type = "gunner back"
 	cannon1.visible = false
-func _on_auto_miner_evolution_button_down() -> void:
-	quadro_dig_evolution.visible = false
-	teamwork_evolution.visible = false
-	sniper_back_evolution.visible = false
-	gunner_back_evolution.visible = false
-	auto_miner_evolution.visible = false
+func evolve_auto_miner() -> void:
+	
 	chisel1.position = Vector2(22.7, 0.4)
 	chisel1.rotation_degrees = 0
 	auto_turret1.visible = true
@@ -329,10 +223,8 @@ func _on_auto_miner_evolution_button_down() -> void:
 	mech_type = "auto miner"
 	cannon1.visible = false
 	chisel2.visible = false
-func _on_quadro_dig_evolution_button_down() -> void:
-	quadro_dig_evolution.visible = false
-	teamwork_evolution.visible = false
-	auto_miner_evolution.visible = false
+func evolve_quadro_dig() -> void:
+	
 	chisel3.visible = true
 	chisel4.visible = true
 	cannon1.visible = true
@@ -350,10 +242,8 @@ func _on_quadro_dig_evolution_button_down() -> void:
 	chisel1.position = Vector2(17.1, 16.4)
 	chisel1.rotation_degrees = 45
 	mech_type = "quadro dig"
-func _on_teamwork_evolution_button_down() -> void:
-	quadro_dig_evolution.visible = false
-	teamwork_evolution.visible = false
-	auto_miner_evolution.visible = false
+func evolve_teamwork() -> void:
+	
 	chisel2.visible = false
 	cannon1.visible = true
 	cannon1.position = Vector2(12.7, 14.2)
@@ -361,9 +251,8 @@ func _on_teamwork_evolution_button_down() -> void:
 	cannon1.reload = 1
 	cannon1.speed = 1
 	mech_type = "teamwork"
-func _on_rocketship_evolution_button_down() -> void:
-	twin_sniper_evolution.visible = false
-	rocketship_evolution.visible = false
+func evolve_rocketship() -> void:
+	
 	cannon1.visible = true
 	cannon1.position = Vector2(16.4, -0.5)
 	cannon1.rotation = 0
@@ -379,9 +268,8 @@ func _on_rocketship_evolution_button_down() -> void:
 	thruster.force = 15
 	thruster.visible = true
 	mech_type = "rocketship"
-func _on_twin_sniper_evolution_button_down() -> void:
-	twin_sniper_evolution.visible = false
-	rocketship_evolution.visible = false
+func evolve_twin_sniper() -> void:
+	
 	chisel1.position = Vector2(-19.5, 1.3)
 	chisel1.rotation_degrees = 180
 	chisel1.visible = true
@@ -401,8 +289,8 @@ func _on_twin_sniper_evolution_button_down() -> void:
 	cannon2.damage = 1.3
 	
 	mech_type = "twin sniper"
-func _on_excavator_evolution_button_down() -> void:
-	excavator_evolution.visible = false
+func evolve_excavator() -> void:
+	
 	chisel3.visible = false
 	chisel4.visible = false
 	chisel2.visible = false
@@ -422,3 +310,4 @@ func _on_excavator_evolution_button_down() -> void:
 	cannon1.visible = false
 	thruster.visible = true
 	mech_type = "Excavator"
+	
--- scenes/player mech.gd
+++ scenes/player mech.gd
@@ -1,2 +1,157 @@
 extends Mech
 class_name PlayerMech
+
+onready var health_loss_particle = $"UI/health loss particle"
+onready var food_bar = $UI/food
+onready var health_bar = $UI/health
+onready var UI = $UI
+onready var XP_bar = $UI/XP
+onready var XP_label = $UI/XPLabel
+onready var max_health_bar = $"UI/max health"
+onready var bullet_speed_bar = $"UI/bullet speed"
+onready var bullet_damage_bar = $"UI/bullet damage"
+onready var bullet_reload_bar = $"UI/bullet reload"
+onready var hunger_endurance_bar = $"UI/hunger endurance"
+onready var chisel_speed_bar = $"UI/chisel speed"
+onready var speed_bar = $"UI/speed"
+onready var stat_coin_label = $"UI/stat coin label"
+onready var back_miner_evolution = $"UI/back miner evolution"
+onready var twin_miner_evolution = $"UI/twin miner evolution"
+onready var sniper_back_evolution = $"UI/sniper back evolution"
+onready var auto_miner_evolution = $"UI/auto miner evolution"
+onready var gunner_back_evolution = $"UI/gunner back evolution"
+onready var quadro_dig_evolution = $"UI/quadro dig evolution"
+onready var teamwork_evolution = $"UI/teamwork evolution"
+onready var twin_sniper_evolution = $"UI/twin sniper evolution"
+onready var rocketship_evolution = $"UI/rocketship evolution"
+onready var excavator_evolution = $"UI/excavator evolution"
+
+func _process(delta: float) -> void:
+	max_health_bar.value = max_health
+	bullet_speed_bar.value = bullet_speed
+	bullet_damage_bar.value = bullet_damage
+	bullet_reload_bar.value = bullet_reload
+	hunger_endurance_bar.value = hunger_endurance
+	chisel_speed_bar.value = chisel_speed
+	speed_bar.value = speed
+	health_bar.value = health
+	food_bar.value = food
+	XP_bar.value = XP
+	XP_label.text = str(level)
+	stat_coin_label.text = str(stat_coins)
+	health_bar.max_value = (max_health * 10) + 100
+	food_bar.max_value = (hunger_endurance * 10) + 100
+	if food > food_bar.max_value:
+		food = food_bar.max_value
+	if XP >= XP_bar.max_value and XP_label.text != str(30):
+		XP = XP - XP_bar.max_value
+		
+		XP_bar.max_value *= 1.1
+		XP_bar.max_value = round(XP_bar.max_value)
+		level += 1
+		stat_coins += 1
+		$UI/Xp_particle.emitting = true
+		
+		if level == 10:
+			twin_miner_evolution.visible = true
+			back_miner_evolution.visible = true
+		if level == 20:
+			if mech_type == "back miner":
+				sniper_back_evolution.visible = true
+				gunner_back_evolution.visible = true
+				auto_miner_evolution.visible = true
+			else:
+				teamwork_evolution.visible = true
+				quadro_dig_evolution.visible = true
+				auto_miner_evolution.visible = true
+		if level == 30:
+			if mech_type == "sniper back":
+				rocketship_evolution.visible = true
+				twin_sniper_evolution.visible = true
+			if mech_type == "quadro dig":
+				excavator_evolution.visible = true
+			XP_label.visible = false
+			XP_bar.visible = false
+			
+func _on_health_timer_timeout() -> void:
+	._on_health_timer_timeout()
+	if food <= 0:
+		health_loss_particle.emitting = true
+	else:
+		health_loss_particle.emitting = false
+func _on_chisel_speed_button_button_down() -> void:
+	if stat_coins > 0 and chisel_speed != 10:
+		chisel_speed += 1
+		stat_coins -= 1
+func _on_hunger_endurance_button_button_down() -> void:
+	if stat_coins > 0 and hunger_endurance != 10:
+		hunger_endurance += 1
+		stat_coins -= 1
+		food += 10
+func _on_bullet_reload_button_button_down() -> void:
+	if stat_coins > 0 and bullet_reload != 10:
+		bullet_reload += 1
+		stat_coins -= 1
+func _on_bullet_damage_button_button_down() -> void:
+	if stat_coins > 0 and bullet_damage != 10:
+		bullet_damage += 1
+		stat_coins -= 1
+func _on_bullet_speed_button_button_down() -> void:
+	if stat_coins > 0 and bullet_speed != 10:
+		bullet_speed += 1
+		stat_coins -= 1
+func _on_speed_button_button_down() -> void:
+	if stat_coins > 0 and speed != 10:
+		speed += 1
+		stat_coins -= 1
+func _on_max_health_button_button_down() -> void:
+	if stat_coins > 0 and max_health != 10:
+		max_health += 1
+		stat_coins -= 1
+		health += 10
+func _on_twin_miner_evolution_button_down() -> void:
+	twin_miner_evolution.visible = false
+	back_miner_evolution.visible = false
+	evolve_twin_miner()
+func _on_back_miner_evolution_button_down() -> void:
+	twin_miner_evolution.visible = false
+	back_miner_evolution.visible = false
+	evolve_back_miner()
+func _on_sniper_back_evolution_button_down() -> void:
+	sniper_back_evolution.visible = false
+	gunner_back_evolution.visible = false
+	auto_miner_evolution.visible = false
+	evolve_sniper_back()
+func _on_gunner_back_evolution_button_down() -> void:
+	sniper_back_evolution.visible = false
+	gunner_back_evolution.visible = false
+	auto_miner_evolution.visible = false
+	evolve_gunner_back()
+func _on_auto_miner_evolution_button_down() -> void:
+	quadro_dig_evolution.visible = false
+	teamwork_evolution.visible = false
+	sniper_back_evolution.visible = false
+	gunner_back_evolution.visible = false
+	auto_miner_evolution.visible = false
+	evolve_auto_miner()
+func _on_quadro_dig_evolution_button_down() -> void:
+	quadro_dig_evolution.visible = false
+	teamwork_evolution.visible = false
+	auto_miner_evolution.visible = false
+	evolve_quadro_dig()
+func _on_teamwork_evolution_button_down() -> void:
+	quadro_dig_evolution.visible = false
+	teamwork_evolution.visible = false
+	auto_miner_evolution.visible = false
+	evolve_teamwork()
+func _on_rocketship_evolution_button_down() -> void:
+	twin_sniper_evolution.visible = false
+	rocketship_evolution.visible = false
+	evolve_rocketship()
+func _on_twin_sniper_evolution_button_down() -> void:
+	twin_sniper_evolution.visible = false
+	rocketship_evolution.visible = false
+	evolve_twin_sniper()
+func _on_excavator_evolution_button_down() -> void:
+	excavator_evolution.visible = false
+	evolve_excavator()
