e907cd98936f535552066a2f9271846a2b874920
Z53-INCOMING <75651517+Z53-INCOMING@users.noreply.github.com>
2021-04-19 11:47:21 -0700

    I removed the boulder debug
    now there isn't a boulder in the wall.

--- code/TileMap.gd
+++ code/TileMap.gd
@@ -103,7 +103,7 @@ func _process(delta: float) -> void:
 
 
 func _on_Area2D_tutorial_complete() -> void:
-	mech.position = $Boulder.position
+	mech.position = mech_position
 	mech.food = 100
 	mech.speed = 0
 	mech.XP = 0
